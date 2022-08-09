d48b71cf21f4326e1dd1d0e1b4111fee90ed0443
SEEKER3 <47914000+2128cz@users.noreply.github.com>
2022-08-09 22:21:06 +0800

    Add files via upload
    test0.22

--- Private/GeneralPlugLibrary/ItemManagementComponent.cpp
+++ Private/GeneralPlugLibrary/ItemManagementComponent.cpp
@@ -34,19 +34,24 @@ void UItemManagementComponent::TickComponent(float DeltaTime, ELevelTick TickTyp
 }
 
 
+// === 1号容器操作 ===|TODO
 
+// 对象容器按实例添加
 bool UItemManagementComponent::V1_AddActorByInst(AActor* ActorInstance)
 {
-	if (ActorInstance && !ActorInstance->IsPendingKill()) {
+	// ActorInstance->IsPendingKill();
+	if (ActorInstance && !IsValid(ActorInstance)) {
 		int32 Eleindex = InstanceContainer.Find(ActorInstance);
 		if (Eleindex >= 0) return false;
-		InstanceContainer.Emplace(ActorInstance);
+		InstanceContainer.Add(ActorInstance);
+		if (!ActorInstance->OnDestroyed.IsAlreadyBound(this, &UItemManagementComponent::V1_OnDestroyed))
 		ActorInstance->OnDestroyed.AddDynamic(this, &UItemManagementComponent::V1_OnDestroyed);
 		return true;
 	}
 	return false;
 }
 
+// 对象容器按实例移除
 bool UItemManagementComponent::V1_RemoveActorByInst(AActor* ActorInstance)
 {
 	if (ActorInstance) {
@@ -57,11 +62,13 @@ bool UItemManagementComponent::V1_RemoveActorByInst(AActor* ActorInstance)
 	return false;
 }
 
+// 获取对象容器实例数组
 TArray<AActor*> UItemManagementComponent::V1_GetActorInst()
 {
 	return InstanceContainer;
 }
 
+// 清空对象容器实例组
 void UItemManagementComponent::V1_CleanActorInst()
 {
 	for (AActor* actor_var: InstanceContainer)
@@ -69,10 +76,124 @@ void UItemManagementComponent::V1_CleanActorInst()
 	InstanceContainer.Empty();
 }
 
+// === 2号容器操作 ===|TODO
+
+// 类容器按实例添加
+bool UItemManagementComponent::V2_AddAClassByInst(AActor* ActorInstance, bool AndDestroy)
+{
+	if (ActorInstance && !IsValid(ActorInstance)) {
+		V2_AddAClassByClass(ActorInstance->GetClass(), 1);
+		if (AndDestroy) ActorInstance->Destroy();
+		return true;
+	}
+	return false;
+}
+
+// 类容器按实例移除
+bool UItemManagementComponent::V2_RemoveAClassByInst(AActor* ActorInstance)
+{
+	if (ActorInstance && !IsValid(ActorInstance)) {
+		V2_RemoveAClassByClass(ActorInstance->GetClass(), 1);
+		return true;
+	}
+	return false;
+}
+
+// 类容器按类数量增加
+void UItemManagementComponent::V2_AddAClassByClass(UClass* ActorClass, int32 Number)
+{
+	const int32* findindex = ClassContainer.Find(ActorClass);
+	if (findindex >= 0) {
+		TArray<int32> values;
+		ClassContainer.GenerateValueArray(values);
+		values[*findindex] += MaxElement(Number, 0);
+	}	
+	ClassContainer.Add(ActorClass, MaxElement(Number, 0));
+}
+
+// 类容器按类数量减少
+void UItemManagementComponent::V2_RemoveAClassByClass(UClass* ActorClass, int32 Number)
+{
+	const int32* findindex = ClassContainer.Find(ActorClass);
+	if (findindex >= 0) {
+		TArray<int32> values;
+		ClassContainer.GenerateValueArray(values);
+		values[*findindex] -= MaxElement(Number, 0);
+		if (values[*findindex] <= 0) {
+			ClassContainer.Remove(ActorClass);
+		}
+	}
+}
+
+// 类容器按类数量设置
+void UItemManagementComponent::V2_SetAClassByClass(UClass* ActorClass, int32 Number, bool SetNumber = true)
+{
+	const int32* findindex = ClassContainer.Find(ActorClass);
+	if (findindex >= 0) {
+		TArray<int32> values;
+		ClassContainer.GenerateValueArray(values);
+		values[*findindex] = SetNumber ? MaxElement(Number, 0) : Number;
+		if (values[*findindex] <= 0) {
+			ClassContainer.Remove(ActorClass);
+		}
+	}
+}
+
+// 类容器按类数量添加
+void UItemManagementComponent::V2_AddAClassByIndex(int32 Index, int32 Number)
+{
+	TArray<UClass*> keys;
+	ClassContainer.GenerateKeyArray(keys);
+	V2_AddAClassByClass(keys[Index], Number);
+}
+
+// 类容器按类数量减少
+void UItemManagementComponent::V2_RemoveAClassByIndex(int32 Index, int32 Number)
+{
+	TArray<UClass*> keys;
+	ClassContainer.GenerateKeyArray(keys);
+	V2_RemoveAClassByClass(keys[Index], Number);
+}
+
+// 类容器按类数量设置
+void UItemManagementComponent::V2_SetAClassByIndex(int32 Index, int32 Number, bool SetNumber = true)
+{
+	TArray<UClass*> keys;
+	ClassContainer.GenerateKeyArray(keys);
+	V2_SetAClassByClass(keys[Index], Number, SetNumber);
+}
+
+// 类容器按实例获取
+bool UItemManagementComponent::V2_GetAClassByInst(AActor* ActorClass, int32*& Number)
+{
+	return V2_GetAClassByClass(ActorClass->GetClass(), Number);
+}
+
+// 类容器按类获取
+bool UItemManagementComponent::V2_GetAClassByClass(UClass* ActorClass, int32*& Number)
+{
+	Number = ClassContainer.Find(ActorClass);
+	return Number >= 0;
+}
+
+// 类容器按索引获取
+bool UItemManagementComponent::V2_GetAClassByIndex(int32 Index, UClass*& ActorClass, int32*& Number)
+{
+	TArray<UClass*> keys;
+	ClassContainer.GenerateKeyArray(keys);
+	TArray<int32> values;
+	ClassContainer.GenerateValueArray(values);
+	ActorClass = keys[Index];
+	Number = values[Index];
+	return Index <= ClassContainer.Num() && Index >= 0;
+}
+
+// 当1号容器内实例发生销毁事件 TODO
 void UItemManagementComponent::V1_OnDestroyed(AActor* DestroyedActor)
 {
 	InstanceContainer.Remove(DestroyedActor);
-	DestroyedActor->OnDestroyed.RemoveDynamic(this, &UItemManagementComponent::V1_OnDestroyed);
+	//V1_WhenDestructionArrive.Broadcast(DestroyedActor);
+	//DestroyedActor->OnDestroyed.RemoveDynamic(this, &UItemManagementComponent::V1_OnDestroyed);
 	TESTFUNC();
 }
 
--- Public/GeneralPlugLibrary/ItemManagementComponent.h
+++ Public/GeneralPlugLibrary/ItemManagementComponent.h
@@ -7,6 +7,9 @@
 #include "ItemManagementComponent.generated.h"
 
 
+
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FWhenDestructionArrive, AActor, OnDestroyed, AActor*, DestroyedActor);
+
 UCLASS( Blueprintable, 
 	ClassGroup=(ItemManagement), 
 	meta = (BlueprintSpawnableComponent),
@@ -29,6 +32,18 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	UFUNCTION()
+		void V1_OnDestroyed(AActor* DestroyedActor);
+
+	//========================================================
+	// TODO
+	// 1号容器：实例容器
+	// 可以使用 V1_AddActorByInst() 和 V1_RemoveActorByInst() 来移除其中的项目
+	// 主要功能是可以在保存实例的同时能够在实例被外部销毁时一同清理此处的项目
+	// 
+	//========================================================
+
 protected:
 	UPROPERTY( EditAnywhere, 
 		BlueprintReadOnly,
@@ -39,6 +54,15 @@ protected:
 	)
 	TArray<AActor*> InstanceContainer;
 
+	UPROPERTY(BlueprintAssignable,
+		Category = "ItemManagement|1_InstanceContainer",
+		meta = (DisplayName = "容器1内发生融毁事件时")
+	)
+	FWhenDestructionArrive V1_WhenDestructionArrive;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "ItemManagement")
+	void TESTFUNC();
+
 	UFUNCTION(BlueprintCallable,
 		Category = "ItemManagement|1_InstanceContainer",
 		meta = (DefaultToSelf),
@@ -47,7 +71,7 @@ protected:
 		meta = (HideSelfPin),
 		//meta = (CompactNodeTitle = "ADD VES1"),
 		meta = (KeyWords = "add set instance 1"), 
-		meta = (ShortToolTip = "向实例容器1添加一个实例"),
+		meta = (ShortToolTip = "向容器添加一个实例"),
 		meta = (ToolTip = "向1号实例容器添加一个实例")
 	)
 	bool V1_AddActorByInst(AActor* ActorInstance);
@@ -60,7 +84,7 @@ protected:
 		meta = (HideSelfPin),
 		//meta = (CompactNodeTitle = "SUB VES1"),
 		meta = (KeyWords = "sub remove instance 1"),
-		meta = (ShortToolTip = "移除容器1中一个对象实例"),
+		meta = (ShortToolTip = "移除容器中一个实例"),
 		meta = (ToolTip = "移除容器1中一个对象实例。")
 	)
 	bool V1_RemoveActorByInst(AActor* ActorInstance);
@@ -68,12 +92,12 @@ protected:
 	UFUNCTION(BlueprintCallable,
 		Category = "ItemManagement|1_InstanceContainer",
 		meta = (DefaultToSelf),
-		meta = (DisplayName = "获取容器1内容"),
+		meta = (DisplayName = "获取容器集合"),
 		//meta = (ExpandEnumAsExecs = ""),
 		meta = (HideSelfPin),
-		meta = (CompactNodeTitle = "GET V1"),
+		meta = (CompactNodeTitle = "GET VES1"),
 		meta = (KeyWords = "get instance 1"),
-		meta = (ShortToolTip = "获取1号实例容器内容"),
+		meta = (ShortToolTip = "获取实例容器内容"),
 		meta = (ToolTip = "获取1号实例容器内容，你也可以直接获取变量 @InstanceContainer")
 	)
 	TArray<AActor*> V1_GetActorInst();
@@ -81,29 +105,186 @@ protected:
 	UFUNCTION(BlueprintCallable,
 		Category = "ItemManagement|1_InstanceContainer",
 		meta = (DefaultToSelf),
-		meta = (DisplayName = "清空容器1"),
+		meta = (DisplayName = "清空容器"),
 		//meta = (ExpandEnumAsExecs = ""),
 		meta = (HideSelfPin),
-		meta = (CompactNodeTitle = "V1 REMOVE"),
+		meta = (CompactNodeTitle = "VES1 REMOVE"),
 		meta = (KeyWords = "Remove clean instance 1"),
 		meta = (ShortToolTip = "1号实例容器清空"),
 		meta = (ToolTip = "清空1号实例容器，这个操作可以同时解放目标的与此处的清理相关绑定")
 	)
 	void V1_CleanActorInst();
 
-	UFUNCTION(BlueprintImplementableEvent, Category = "ItemManagement|1_InstanceContainer")
-		void TESTFUNC();
+	//========================================================
+	// TODO
+	// 2号容器：实例类容器
+	// 可以根据实例的类型来保存一个类索引，每次的实例添加将转为这个类索引下的计数
+	// 
+	//========================================================
 
-	/*DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FWhenDestructionArrive, AActor, OnDestroyed, AActor*, DestroyedActor);
-	UPROPERTY(BlueprintAssignable,
+	UPROPERTY(EditAnywhere,
+		BlueprintReadOnly,
 		Category = "ItemManagement|1_InstanceContainer",
-		meta = (DisplayName = "容器1内发生融毁事件时")
+		meta = (DisplayName = "类容器(容器2)"),
+		meta = (HideAlphaChannel),
+		meta = (NoGetter)
 	)
-		FWhenDestructionArrive V1_WhenDestructionArrive;*/
+		TMap<UClass*, int32> ClassContainer;
 
-private:
-	UFUNCTION()
-	void V1_OnDestroyed(AActor* DestroyedActor);
+
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按实例添加"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		//meta = (CompactNodeTitle = "ADD VES2"),
+		meta = (KeyWords = " add class instance 2"),
+		meta = (ShortToolTip = "2号实例类容器增加"),
+		meta = (ToolTip = "按实例类型向实例类容器添加一个项目，@AndDestroy 可选择删除")
+	)
+		bool V2_AddAClassByInst(AActor* ActorInstance, bool AndDestroy);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按实例移除"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		// meta = (CompactNodeTitle = "REMOVE VES2"),
+		meta = (KeyWords = "remove class instance 2"),
+		meta = (ShortToolTip = "2号实例类容器减少"),
+		meta = (ToolTip = "按实例类型移除实例类容器中的一个项目")
+	)
+		bool V2_RemoveAClassByInst(AActor* ActorInstance);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按类增加"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		//meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "class item add ++ instance 2"),
+		meta = (ShortToolTip = "2号实例类容器增加"),
+		meta = (ToolTip = "按类增加实例类容器中的项目数量，只考虑正数，负数会被忽略")
+	)
+		void V2_AddAClassByClass(UClass* ActorClass, int32 Number);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按类减少"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		//meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "class item sub -- instance 2"),
+		meta = (ShortToolTip = "2号实例类容器减少"),
+		meta = (ToolTip = "按类减少实例类容器中的项目数量，只考虑正数，负数会被忽略")
+	)
+		void V2_RemoveAClassByClass(UClass* ActorClass, int32 Number);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按类设置"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "class item change instance 2"),
+		meta = (ShortToolTip = "2号实例类容器变更"),
+		meta = (ToolTip = "按类设置实例类容器中项目数量，@SetNumber 可选择是否直接设置为数值，否则将基于当前值进行运算")
+	)
+		void V2_SetAClassByClass(UClass* ActorClass, int32 Number, bool SetNumber = true);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按索引增加"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		//meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "index item add ++ instance 2"),
+		meta = (ShortToolTip = "2号实例类容器增加"),
+		meta = (ToolTip = "按索引增加实例类容器中的项目数量，只考虑正数，负数会被忽略")
+	)
+		void V2_AddAClassByIndex(int32 Index, int32 Number);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按索引减少"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		//meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "index item sub -- instance 2"),
+		meta = (ShortToolTip = "2号实例类容器减少"),
+		meta = (ToolTip = "按索引减少实例类容器中的项目数量，只考虑正数，负数会被忽略")
+	)
+		void V2_RemoveAClassByIndex(int32 Index, int32 Number);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "对象类容器2按索引设置"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "index item change instance 2"),
+		meta = (ShortToolTip = "2号实例类容器变更"),
+		meta = (ToolTip = "按索引设置实例类容器中项目数量，@SetNumber 可选择是否直接设置为数值，否则将基于当前值进行运算")
+	)
+		void V2_SetAClassByIndex(int32 Index, int32 Number, bool SetNumber = true);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "按实例获取对象类容器2内容"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		//meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "get instance 2"),
+		meta = (ShortToolTip = "2号实例类容器减少"),
+		meta = (ToolTip = "按索引减少实例类容器中的项目数量，只考虑正数，负数会被忽略")
+	)
+		bool V2_GetAClassByInst(AActor* ActorClass, int32*& Number);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "按类获取对象类容器2内容"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "get instance 2"),
+		meta = (ShortToolTip = "2号实例类容器变更"),
+		meta = (ToolTip = "按索引设置实例类容器中项目数量，@SetNumber 可选择是否直接设置为数值，否则将基于当前值进行运算")
+	)
+		bool V2_GetAClassByClass(UClass* ActorClass, int32*& Number);
+
+	UFUNCTION(BlueprintCallable,
+		Category = "ItemManagement|2_ClassContainer",
+		meta = (DefaultToSelf),
+		meta = (DisplayName = "按索引获取对象类容器2内容"),
+		//meta = (ExpandEnumAsExecs = ""),
+		meta = (HideSelfPin),
+		meta = (CompactNodeTitle = "VES2 "),
+		meta = (KeyWords = "get instance 2"),
+		meta = (ShortToolTip = "2号实例类容器变更"),
+		meta = (ToolTip = "按索引设置实例类容器中项目数量，@SetNumber 可选择是否直接设置为数值，否则将基于当前值进行运算")
+	)
+		bool V2_GetAClassByIndex(int32 Index, UClass*& ActorClass, int32*& Number);
+
+	//========================================================
+	// 
+	// 3号容器：实例容器
+	//  
+	// 
+	//========================================================
+
+
+
 
 };
 
