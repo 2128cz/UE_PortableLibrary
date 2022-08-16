b6402bfb99e496fc6188d5ad8a9cbe04154abadc
SEEKER3 <47914000+2128cz@users.noreply.github.com>
2022-08-16 23:27:20 +0800

    ༼ つ ◕_◕ ༽つ ~插件

---
+++ Source/GeneralPlugLibrary/Private/GeneralPlugLibraryBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GeneralPlugLibraryBPLibrary.h"
+#include "GeneralPlugLibrary.h"
+
+UGeneralPlugLibraryBPLibrary::UGeneralPlugLibraryBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UGeneralPlugLibraryBPLibrary::GeneralPlugLibrarySampleFunction(float Param)
+{
+	return -1;
+}
+
